5b9166a670398c76dd9d19f357a283701298b967
Frankline Were <franklinegift@gmail.com>
2024-07-10 21:14:04 +0300

    filter drivers

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, driverName) {
+    return drivers.filter(driver => driver.toLowerCase() === driverName.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letter) {
+    return drivers.filter(driver => driver.startsWith(letter));
+}
+
+function matchName(drivers, driverName) {
+    return drivers.filter(driver => driver.name === driverName);
+}
